2382f33f6d7aab8ba3fe2f9c0035fa8660f08531
Dieu Nguyen <dieunb@gmail.com>
2020-04-26 14:34:31 +0700

    Add delete book handler

--- routing/main.go
+++ routing/main.go
@@ -11,6 +11,7 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/books/{title}/page/{page}", BookHandler).Methods("GET")
+	r.HandleFunc("/books/{title}", DeleteBookHandler).Methods("DELETE")
 
 	http.ListenAndServe(":4000", r)
 }
@@ -21,4 +22,11 @@ func BookHandler(w http.ResponseWriter, r *http.Request) {
 	page := vars["page"]
 
 	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+}
+
+func DeleteBookHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+
+	fmt.Fprintf(w, "Book %s have deleted!", title)
 }
