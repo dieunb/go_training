b4f96c5c90e30951952cd66411de62c07544b51b
Dieu Nguyen <dieunb@gmail.com>
2020-04-26 22:06:57 +0700

    Render to view

--- main.go
+++ main.go
@@ -3,17 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
 
+type Book struct {
+	Title string
+}
+
+type BookToRead struct {
+	PageTitle string
+	Books     []Book
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	r.HandleFunc("/", BookToReadHandler).Methods("GET")
 
 	r.HandleFunc("/books/{title}/page/{page}", BookHandler).Methods("GET")
 	r.HandleFunc("/books/{title}", DeleteBookHandler).Methods("DELETE")
 
 	http.ListenAndServe(":4000", r)
+}
+
+func BookToReadHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+
+	data := BookToRead{
+		PageTitle: "My books list",
+		Books: []Book{
+			{Title: "Ruby"},
+			{Title: "Ruby on Rails"},
+			{Title: "Go"},
+		},
+	}
+	tmpl.Execute(w, data)
 }
 
 func BookHandler(w http.ResponseWriter, r *http.Request) {
