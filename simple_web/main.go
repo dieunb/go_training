3560f041097b80d4bd76505c0c56e267e95ba88a
Dieu Nguyen <dieunb@gmail.com>
2020-04-26 11:55:00 +0700

    Serve static assets

--- simple_web/main.go
+++ simple_web/main.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you are requested: %s\n", r.URL.Path)
+		fmt.Fprint(w, "Welcome to my website")
 	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.ListenAndServe(":4000", nil)
 }
