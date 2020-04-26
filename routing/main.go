2d8f5059cb38b12d909001703bd1db780913a4e4
Dieu Nguyen <dieunb@gmail.com>
2020-04-26 14:27:20 +0700

    Split serve request to a new handler function

--- routing/main.go
+++ routing/main.go
@@ -10,13 +10,15 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
-
-		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
-	})
+	r.HandleFunc("/books/{title}/page/{page}", BookHandler)
 
 	http.ListenAndServe(":4000", r)
+}
+
+func BookHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	page := vars["page"]
+
+	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 }
